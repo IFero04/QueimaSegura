c532fb91a43a1eb7ddc16e84e57228c3b4a36ff4
Pin7i7o <118998982+Pin7i7o@users.noreply.github.com>
2024-06-04 14:49:37 +0100

    New request perm fragment setup

---
+++ app/src/main/java/com/example/queimasegura/common/reqPerm/RequestActivity.kt
@@ -0,0 +1,99 @@
+package com.example.queimasegura.common.reqPerm
+
+import android.os.Bundle
+import android.widget.RadioGroup
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.queimasegura.R
+import com.example.queimasegura.common.reqPerm.fragment.MapFragment
+import com.example.queimasegura.common.reqPerm.fragment.PostCodeFragment
+import com.example.queimasegura.common.reqPerm.fragment.QueimadaFragment
+import com.example.queimasegura.common.reqPerm.fragment.SuppTeamFragment
+
+class RequestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_request)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.permissionRequestPage)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        init()
+    }
+
+    private fun init() {
+        findViewById<RadioGroup>(R.id.radioGroupType).setOnCheckedChangeListener { _, checkedId ->
+            handleTypeRadioGroupChange(checkedId)
+        }
+
+        findViewById<RadioGroup>(R.id.radioGroupSuppTeam).setOnCheckedChangeListener { _, checkedId ->
+            handleSuppTeamRadioGroupChange(checkedId)
+        }
+
+        findViewById<RadioGroup>(R.id.radioGroupLocation).setOnCheckedChangeListener { _, checkedId ->
+            handleLocationRadioGroupChange(checkedId)
+        }
+    }
+
+    private fun handleTypeRadioGroupChange(checkedId: Int) {
+        val fragment = when (checkedId) {
+            R.id.radioButtonQueimada -> QueimadaFragment()
+            else -> null
+        }
+
+        if (fragment != null) {
+            replaceFragment(fragment, R.id.fragmentContainerViewType)
+        } else {
+            removeFragment(R.id.fragmentContainerViewType)
+        }
+    }
+
+    private fun handleSuppTeamRadioGroupChange(checkedId: Int) {
+        val fragment = when (checkedId) {
+            R.id.radioButtonYes -> SuppTeamFragment()
+            else -> null
+        }
+
+        if (fragment != null) {
+            replaceFragment(fragment, R.id.fragmentContainerViewSuppTeam)
+        } else {
+            removeFragment(R.id.fragmentContainerViewSuppTeam)
+        }
+    }
+
+    private fun handleLocationRadioGroupChange(checkedId: Int) {
+        val fragment = when (checkedId) {
+            R.id.radioButtonPostCode -> PostCodeFragment()
+            R.id.radioButtonMap -> MapFragment()
+            else -> null
+        }
+
+        if (fragment != null) {
+            replaceFragment(fragment, R.id.fragmentContainerViewLocation)
+        } else {
+            removeFragment(R.id.fragmentContainerViewLocation)
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment, containerId: Int) {
+        supportFragmentManager.beginTransaction()
+            .replace(containerId, fragment)
+            .commit()
+    }
+
+    private fun removeFragment(containerId: Int) {
+        val fragment = supportFragmentManager.findFragmentById(containerId)
+        fragment?.let {
+            supportFragmentManager.beginTransaction()
+                .remove(it)
+                .commit()
+        }
+    }
+}
