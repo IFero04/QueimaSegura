e23ac547b30cd790f3546cbd1711cb99a140d16e
Pin7i7o <118998982+Pin7i7o@users.noreply.github.com>
2024-06-05 17:53:29 +0100

    Added dropdowns menus to perm_req page

--- app/src/main/java/com/example/queimasegura/common/reqPerm/fragment/SuppTeamFragment.kt
+++ app/src/main/java/com/example/queimasegura/common/reqPerm/fragment/SuppTeamFragment.kt
@@ -3,8 +3,12 @@ package com.example.queimasegura.common.reqPerm.fragment
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.appcompat.widget.PopupMenu
 import com.example.queimasegura.R
 
 class SuppTeamFragment : Fragment() {
@@ -13,7 +17,28 @@ class SuppTeamFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_supp_team, container, false)
+        val view = inflater.inflate(R.layout.fragment_supp_team, container, false)
+
+        view.findViewById<ImageButton>(R.id.imageButtonDropDownSuppTeam).setOnClickListener {
+            handleDropDownMenu(it, R.menu.temp_dropdown_supp_team)
+        }
+
+        return  view
+    }
+
+    private fun handleDropDownMenu(view: View, menuId: Int) {
+        val popupMenu = PopupMenu(requireContext(), view)
+        popupMenu.menuInflater.inflate(menuId, popupMenu.menu)
+
+        popupMenu.setOnMenuItemClickListener { item: MenuItem ->
+            showToast(item.title.toString())
+            true
+        }
+
+        popupMenu.show()
+    }
+
+    private fun showToast(str: String) {
+        Toast.makeText(requireContext(), str, Toast.LENGTH_SHORT).show()
     }
 }
