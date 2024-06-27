43a0d79a597ed6ee945ece772b967662a70c3498
Afonso Faria <afonsofariatech@gmail.com>
2024-06-27 20:47:14 +0100

    Fix ADMIN

--- app/src/main/java/com/example/queimasegura/admin/fragments/users/UsersFragment.kt
+++ app/src/main/java/com/example/queimasegura/admin/fragments/users/UsersFragment.kt
@@ -2,6 +2,7 @@ package com.example.queimasegura.admin.fragments.users
 
 import android.app.AlertDialog
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.MenuItem
@@ -233,6 +234,8 @@ class UsersFragment : Fragment() {
                 else -> 0
             }
 
+            Log.d("USER TYPE", userType.toString())
+            Log.d("USER TYPE", selectedPermission)
             viewModel.editUserPerms(authUser, userList[position].id, userType)
             adapter.updateUserPermission(position, userType)
             dialog.dismiss()
